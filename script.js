c6ebc887e65ebeaa83cca2123ba348776f001fb0
saqibjvd <70890519+saqibjvd@users.noreply.github.com>
2023-05-02 16:01:34 +0100

    movie card part 1

--- script.js
+++ script.js
@@ -7,6 +7,24 @@ function setup() {
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
   rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+
+  // MOVIE CARD - Display episode, Name  - part 1 (100 episodes)
+
+  for (let i = 0; i < episodeList.length; i++) {
+    let topContainer = document.createElement("div");
+    topContainer.classList.add("top-container");
+    rootElem.appendChild(topContainer);
+
+    let movieCard = document.createElement("div");
+    movieCard.classList.add("movie-card");
+    topContainer.appendChild(movieCard);
+
+    let episodeName = document.createElement("p");
+    movieCard.appendChild(episodeName);
+    episodeName.innerText = episodeList[i].name;
+  }
+
+  console.log(episodeList);
 }
 
 window.onload = setup;
